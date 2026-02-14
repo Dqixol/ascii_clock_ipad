c8692ce1fe75aa9130a8d7821eadcaa8034ad7ea
Qichen Dong <qichen.dong@cern.ch>
2026-02-14 23:45:07 +0000

    add weather

--- main.py
+++ main.py
@@ -112,41 +112,71 @@ def colour_art(art, color):
 def remove_dots(art):
     return art.replace(" d8b ", "     ").replace(" Y8P ", "     ")
 
+def getWeather():
+    # weather = open('/Users/jvshang/Documents/ascii_clock_ipad/example_weather.txt').read()
+    weather = os.popen('curl -fGsS --compressed "wttr.in/BS1+1NR?1F"').read()
+    weather_now = weather.splitlines()[2:7]
+    weather_now = '\n'.join(weather_now)
+    weather_forcast = weather.splitlines()[7:17]
+    weather_forcast = '\n'.join(weather_forcast)
+    location = weather.splitlines()[17][10:][:-22]
+    if weather_forcast.strip() == '':
+        raise
+    return weather_now, weather_forcast, location
+
+def getLunarChineseChars(month, day):
+    chinese_months = ["正月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "冬月", "腊月"]
+    chinese_days = ["初一", "初二", "初三", "初四", "初五", "初六", "初七", "初八", "初九", "初十",
+                    "十一", "十二", "十三", "十四", "十五", "十六", "十七", "十八", "十九", "二十",
+                    "廿一", "廿二", "廿三", "廿四", "廿五", "廿六", "廿七", "廿八", "廿九", "三十"
+                    ]
+    return chinese_months[month-1] + chinese_days[day-1]
+
 def main():
     os.system('clear')
     date_str = None
-    width_saved, height_saved = os.get_terminal_size()
     dot_printed = False
-    flag_resize = False
+    last_weather_update = datetime.datetime.now() - datetime.timedelta(days=1)
     while True:
         width, height = os.get_terminal_size()
-        if (width, height) != (width_saved, height_saved):
-            flag_resize = True
-        if width < 100 or height < 30:
-            print(f"({width}x{height}) Please resize your terminal to at least 100x30 for optimal display.") 
+        if width < 125 or height < 30:
+            print(f"({width}x{height}) Please resize your terminal to at least 125x30 for optimal display.") 
             sleep(1)
             os.system('clear') 
             continue
-        if flag_resize or ((not date_str) or (date_str != datetime.datetime.now().strftime(f"%d{determine_th_st_nd_rd(datetime.datetime.now().day)}  %b"))):
-            os.system('clear')
-            date_str = datetime.datetime.now() .strftime(f"%d{determine_th_st_nd_rd(datetime.datetime.now().day)}  %b") 
-            lunar_date = LunarDate.today()
-            date_art       = colour_art(remove_trailing_newlines(centre_art(             text2art(date_str,        font='colossal'), width-1), 4), bcolors.WHITE)
-            lunar_date_art = colour_art(remove_trailing_newlines(centre_art(assemble_chinese_date(lunar_date.month, lunar_date.day), width-1), 3), bcolors.YELLOW)
-            print('\n' + date_art + lunar_date_art)
+        if datetime.datetime.now() - last_weather_update > datetime.timedelta(minutes=30):
+            try:
+                weather_now, weather_forcast, location = getWeather()
+                last_weather_update = datetime.datetime.now()
+            except:
+                print('failed to get weather, will retry')
+        os.system('clear')
+        date_str = datetime.datetime.now() .strftime(f"%d{determine_th_st_nd_rd(datetime.datetime.now().day)}  %b") 
+        date_art       = text2art(date_str, font='colossal')
+        lunar_date = LunarDate.today()
+        lunar_date_str = getLunarChineseChars(lunar_date.month, lunar_date.day)
+        lunar_date_art = (lunar_date_str + '\n')* 8
+
+        # lunar_date_art = colour_art(remove_trailing_newlines(centre_art(assemble_chinese_date(lunar_date.month, lunar_date.day), width-1), 3), bcolors.YELLOW)
+        print('\n' + colour_art(combine_arts(['                  \n'* 8, date_art, '   │     \n'*8, lunar_date_art]), bcolors.CYAN) + '\n\n')
+        try:
+            print(centre_art(location, width-1))
+            print(combine_arts([' \n'* 10, weather_forcast])+'\n\n')
+        except:
+            print('\n' + centre_art("Failed to get weather data, will retry", width-1))
         time_str = datetime.datetime.now() .strftime("%H : %M")
-        time_art = colour_art(remove_trailing_newlines(centre_art(             text2art(time_str,        font='colossal'), width-1), 0), bcolors.GREEN)
+        time_art = text2art(time_str,        font='colossal')
         if not dot_printed:
             dot_printed = True
         else: 
             time_art = remove_dots(time_art)
             dot_printed = False
-        print(time_art)
-        sleep(60)
-        sys.stdout.write('\033[8A')
-        sys.stdout.flush()
-
-
+        try: 
+            weather_now_print = f'                Updated {(datetime.datetime.now() - last_weather_update).total_seconds():.0f}s ago\n' + weather_now
+            print(combine_arts(['                  \n'* 8, time_art, '   │\n'*8, '\n'*2+weather_now_print]))
+        except:
+            print(combine_arts(['                  \n'* 8, time_art]))
+        sleep(1)
+        
 if __name__ == "__main__":
     main()
-
