d63bad20e5cede40177f1892b78186a5528c2e5e
Qichen Dong <qichen.dong@cern.ch>
2026-02-15 09:30:10 +0000

    better

--- main.py
+++ main.py
@@ -106,7 +106,9 @@ def centre_art(art, width):
     return "\n".join(centred_lines)
 
 def colour_art(art, color):
-    colored_art = f"{color}{bcolors.BOLD}{art}{bcolors.ENDC}" 
+    colored_art = ''
+    for line in art.splitlines():
+        colored_art += f"{color}{bcolors.BOLD}{line.rstrip()}{bcolors.ENDC}\n"
     return colored_art
 
 def remove_dots(art):
@@ -114,12 +116,12 @@ def remove_dots(art):
 
 def getWeather():
     # weather = open('/Users/jvshang/Documents/ascii_clock_ipad/example_weather.txt').read()
-    weather = os.popen('curl -fGsS --compressed "wttr.in/BS1+1NR?1F"').read()
+    weather = os.popen('curl -fGsS --compressed "wttr.in/BS1+1NR?2F"').read()
     weather_now = weather.splitlines()[2:7]
     weather_now = '\n'.join(weather_now)
-    weather_forcast = weather.splitlines()[7:17]
+    weather_forcast = weather.splitlines()[7:27]
     weather_forcast = '\n'.join(weather_forcast)
-    location = weather.splitlines()[17][10:][:-22]
+    location = weather.splitlines()[27][10:][:-22]
     if weather_forcast.strip() == '':
         raise
     return weather_now, weather_forcast, location
@@ -139,8 +141,8 @@ def main():
     last_weather_update = datetime.datetime.now() - datetime.timedelta(days=1)
     while True:
         width, height = os.get_terminal_size()
-        if width < 125 or height < 30:
-            print(f"({width}x{height}) Please resize your terminal to at least 125x30 for optimal display.") 
+        if width != 128 or height != 44:
+            print('\n'*int(height / 2 - 1) + f"({width}x{height}) Please resize your terminal to exactly 128*44") 
             sleep(1)
             os.system('clear') 
             continue
@@ -148,35 +150,39 @@ def main():
             try:
                 weather_now, weather_forcast, location = getWeather()
                 last_weather_update = datetime.datetime.now()
-            except:
-                print('failed to get weather, will retry')
-        os.system('clear')
-        date_str = datetime.datetime.now() .strftime(f"%d{determine_th_st_nd_rd(datetime.datetime.now().day)}  %b") 
+            except Exception as error:
+                print(error)
+                print('failed to get weather, will retry...')
+        date_str = datetime.datetime.now() .strftime(f"%a  %d{determine_th_st_nd_rd(datetime.datetime.now().day)}  %b") 
         date_art       = text2art(date_str, font='colossal')
         lunar_date = LunarDate.today()
         lunar_date_str = getLunarChineseChars(lunar_date.month, lunar_date.day)
-        lunar_date_art = ' \n'*7 + (lunar_date_str + '\n')
-
-        # lunar_date_art = colour_art(remove_trailing_newlines(centre_art(assemble_chinese_date(lunar_date.month, lunar_date.day), width-1), 3), bcolors.YELLOW)
-        print('\n' + colour_art(combine_arts(['                  \n'* 8, date_art, '   │    \n'*8, lunar_date_art]), bcolors.CYAN) + '\n')
-        try:
-            print(centre_art(location, width-1))
-            print(combine_arts([' \n'* 10, weather_forcast])+'\n\n')
-        except:
-            print('\n' + centre_art("Failed to get weather data, will retry", width-1))
         time_str = datetime.datetime.now() .strftime("%H : %M")
         time_art = text2art(time_str,        font='colossal')
-        # if not dot_printed:
-        #     dot_printed = True
-        # else: 
-        #     time_art = remove_dots(time_art)
-        #     dot_printed = False
+
+        # lunar_date_art = colour_art(remove_trailing_newlines(centre_art(assemble_chinese_date(lunar_date.month, lunar_date.day), width-1), 3), bcolors.YELLOW)
+        # print('\n' + colour_art(combine_arts(['                  \n'* 8, date_art, '   │    \n'*8, lunar_date_art]), bcolors.CYAN) + '\n')
+        
+        if not dot_printed:
+            dot_printed = True
+        else: 
+            time_art = remove_dots(time_art)
+            dot_printed = False
+        
+        os.system('clear')
+        print('\n' + colour_art(centre_art(combine_arts([date_art, "\n"*8]), width-1), bcolors.CYAN) + '\n')
+        try:
+            print(centre_art(f'{location.strip()}, {lunar_date_str}', width-1))
+            print(combine_arts([' \n'* 20, weather_forcast]) + '\n\n')
+        except Exception as error:
+            print(centre_art(error, width-1))
+            print(centre_art("Failed to get weather data, will retry...", width-1))
         try: 
-            weather_now_print = f'                Updated {(datetime.datetime.now() - last_weather_update).total_seconds():.0f}s ago\n' + weather_now
+            weather_now_print = f'                Updated {(datetime.datetime.now() - last_weather_update).total_seconds() / 60.0:.0f}min ago\n' + weather_now
             print(combine_arts(['                  \n'* 8, time_art, '   │\n'*8, '\n'*2+weather_now_print]))
         except:
-            print(combine_arts(['                  \n'* 8, time_art]))
-        sleep(60)
+            print(centre_art(time_art, width-1))
+        sleep(0.99)
         
 if __name__ == "__main__":
     main()
