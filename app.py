725fc89f4d19c06ebb12552c2e797390f9b77936
Qichen Dong <qichen.dong@cern.ch>
2026-03-27 16:30:00 +0100

    web

---
+++ app.py
@@ -0,0 +1,88 @@
+from flask import Flask, render_template, Response
+from ansi2html import Ansi2HTMLConverter
+import weather
+import datetime
+
+from io import StringIO
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+plt.style.use('dark_background')
+import matplotlib.dates as mdates
+
+app = Flask(__name__)
+conv = Ansi2HTMLConverter(inline=True)
+
+weather_info = weather.weatherInfo("BS1 1NR")
+
+def make_svg():
+    legend_kwargs = {"loc": "upper right", "frameon" : False, "fontsize": 12}
+    df_forcast, df_air_quality = weather_info.getDoI()
+    fig, axs = plt.subplots(2, 2, figsize=(12, 3.5))
+    for ax in axs.flat:
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%Hh'))
+        ax.set_xlim(df_forcast["time"].min(), df_forcast["time"].max())
+    # ax00:
+    for ax in [axs[0,0]]:
+        ax.plot(df_forcast["time"], df_forcast["temperature_2m"], label="Temperature [°C]")
+        ax.plot(df_forcast["time"], df_forcast["apparent_temperature"], label="Feels like [°C]")
+        min_temp = min(df_forcast["temperature_2m"].min(), df_forcast["apparent_temperature"].min())
+        max_temp = max(df_forcast["temperature_2m"].max(), df_forcast["apparent_temperature"].max())
+        ax.set_ylim(0 if min_temp > 0 else min_temp - 1, 20 if max_temp < 20 else max_temp + 1)
+        ax.legend(**legend_kwargs, ncol=2)
+        ax.get_xaxis().set_visible(False)
+    for ax in [axs[0,1]]:
+        ax.plot(df_forcast["time"], df_forcast["precipitation_probability"]/100.0, label="Probability")
+        ax.bar(df_forcast["time"], df_forcast["precipitation"]*4.0, width = 0.005, label="Precipitation [mm/h]")
+        ax.legend(**legend_kwargs, ncol=2)
+        ax.get_xaxis().set_visible(False)
+        ax.set_ylim(0, 1 if df_forcast["precipitation"].max() < 1 else df_forcast["precipitation"].max() + 0.5)
+    for ax in [axs[1, 0]]:
+        ax.plot(df_air_quality["time"], df_air_quality["birch_pollen"], label="Birch Pollen [grains/m³]", alpha=0.0)
+        ax.legend(**legend_kwargs)
+        ax.set_ylim(0, 50 if df_air_quality["birch_pollen"].max() < 50 else df_air_quality["birch_pollen"].max() + 20)
+        df_birch_0_10 = df_air_quality.query("birch_pollen >= 0 and birch_pollen < 10")
+        df_birch_10_20 = df_air_quality.query("birch_pollen >= 10 and birch_pollen < 20")
+        df_birch_20_100 = df_air_quality.query("birch_pollen >= 20 and birch_pollen < 100")
+        df_birch_100 = df_air_quality.query("birch_pollen >= 100")
+        ax.bar(df_birch_0_10["time"],   df_birch_0_10["birch_pollen"],   color="green",  width = 0.005)
+        ax.bar(df_birch_10_20["time"], df_birch_10_20["birch_pollen"], color="yellow", width = 0.005)
+        ax.bar(df_birch_20_100["time"], df_birch_20_100["birch_pollen"], color="red", width = 0.005)
+        ax.bar(df_birch_100["time"],    df_birch_100["birch_pollen"],    color="purple",    width = 0.005)
+    for ax in [axs[1, 1]]:
+        ax.plot(df_air_quality["time"], df_air_quality["european_aqi"], label="European AQI", alpha=0.0)
+        ax.legend(**legend_kwargs)
+        ax.set_ylim(20 if df_air_quality["european_aqi"].min() > 20 else df_air_quality["european_aqi"].min() - 10, 50 if df_air_quality["european_aqi"].max() < 50 else df_air_quality["european_aqi"].max() + 10)
+        df_aqi_0_50 = df_air_quality.query("european_aqi >= 0 and european_aqi < 50")
+        df_aqi_50_100 = df_air_quality.query("european_aqi >= 50 and european_aqi < 100")
+        df_aqi_100_150 = df_air_quality.query("european_aqi >= 100 and european_aqi < 150")
+        df_aqi_150 = df_air_quality.query("european_aqi >= 150")
+        ax.bar(df_aqi_0_50["time"],   df_aqi_0_50["european_aqi"],   color="cyan",  width = 0.005)
+        ax.bar(df_aqi_50_100["time"], df_aqi_50_100["european_aqi"], color="green", width = 0.005)
+        ax.bar(df_aqi_100_150["time"], df_aqi_100_150["european_aqi"], color="yellow", width = 0.005)
+        ax.bar(df_aqi_150["time"],    df_aqi_150["european_aqi"],    color="red",    width = 0.005)
+
+    fig.tight_layout()
+    buf = StringIO()
+    fig.savefig(buf, format="svg", bbox_inches="tight", transparent=True)
+    plt.close(fig)
+
+    return buf.getvalue()
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/art")
+def art():
+    ascii_art = weather_info.getArt()
+    html_art = conv.convert(ascii_art, full=False)
+    return html_art
+
+@app.route("/plot.svg")
+def plot_svg():
+    svg = make_svg()
+    return Response(svg, mimetype="image/svg+xml")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3000)
---
+++ weather.py
@@ -0,0 +1,221 @@
+from art import text2art
+from time import sleep
+import datetime
+import subprocess
+from lunardate import LunarDate
+import sys
+import json
+import pytz
+import itertools
+import pandas as pd
+
+class bcolors:
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    MAGENTA = '\033[95m'
+    CYAN = '\033[96m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    BLINK = '\033[5m'
+    WHITE = '\033[0m'
+
+
+chinese_month_art = """
+  8888888888888
+  8           8
+  8888888888888
+  8           8
+  8888888888888
+ 8            8
+Y            YP
+"""
+
+chinese_day_art ="""
+888888888888888
+8             8
+8             8
+888888888888888
+8             8
+8             8
+888888888888888
+"""
+
+# 农
+chinese_lunar_art ="""
+      d        
+ d88888888888b 
+8   d         8
+   d 8b   PP   
+  d  8 bb P    
+ d   8   bb8888
+     YP        
+"""
+
+chinese_lunar_art = " "* 15 + chinese_lunar_art
+chinese_month_art = " "* 15 + chinese_month_art
+chinese_day_art   = " "* 15 + chinese_day_art
+
+map_chinese_arts = {
+    "month" : chinese_month_art,
+    "day" : chinese_day_art,
+    "0" : text2art("0", font='colossal'),
+    "1" : text2art("1", font='colossal'),
+    "2" : text2art("2", font='colossal'),
+    "3" : text2art("3", font='colossal'),
+    "4" : text2art("4", font='colossal'), 
+    "5" : text2art("5", font='colossal'), 
+    "6" : text2art("6", font='colossal'), 
+    "7" : text2art("7", font='colossal'), 
+    "8" : text2art("8", font='colossal'), 
+    "9" : text2art("9", font='colossal'),
+    "space" : "  \n"* 8
+}
+
+def combine_arts(arts):
+    art_lines = zip(*[art.splitlines() for art in arts])
+    combined_art = "\n".join(" ".join(line) for line in art_lines) 
+    return combined_art
+
+def assemble_chinese_date(month, day):
+    store = []
+    store.append(chinese_lunar_art)
+    store.append(map_chinese_arts["space"])
+    for char in str(month): 
+        store.append(map_chinese_arts[char])
+    store.append(map_chinese_arts["month"]) 
+    store.append(map_chinese_arts["space"])
+    for char in str(day): 
+        store.append(map_chinese_arts[char]) 
+    store.append(map_chinese_arts["day"])
+    return combine_arts(store) + "\n"*2
+
+def determine_th_st_nd_rd(day):
+    if 4 <= day <= 20 or 24 <= day <= 30:
+        return "th" 
+    else: 
+        return ["st", "nd", "rd"][day % 10 - 1]
+
+def remove_trailing_newlines(art, num_newlines=2):
+    art = art.rstrip()
+    art = art + " "* 5
+    art = art + "\n"*num_newlines
+    return art
+
+def centre_art(art, width):
+    art_lines = art.splitlines() 
+    centred_lines = [line.center(width) for line in art_lines] 
+    return "\n".join(centred_lines)
+
+def colour_art(art, color):
+    colored_art = ''
+    for line in art.splitlines():
+        colored_art += f"{color}{bcolors.BOLD}{line.rstrip()}{bcolors.ENDC}\n"
+    return colored_art
+
+def remove_dots(art):
+    return art.replace(" d8b ", "     ").replace(" Y8P ", "     ")
+
+def getWeather(postcode):
+    # return None, '', 'Unknown Location'
+    postcode = postcode.replace(" ", "+")
+    weather = subprocess.getoutput(f'curl -fGsS --compressed "wttr.in/{postcode}?1F"')
+    weather_split = weather.splitlines()
+    if len(weather_split) < 18:
+        return None, '', 'Unknown Location', None
+    weather_now = weather_split[2:7]
+    weather_now = '\n'.join(weather_now)
+    weather_forcast = weather_split[7:17]
+    weather_forcast = '\n'.join(weather_forcast)
+    weather_forcast = weather_forcast[:183] + "   Today    " + weather_forcast[195:]
+    location = weather_split[17].split("[")[0].split(":")[1].strip()
+    gps_coords = weather_split[17].split("[")[1].split("]")[0].split(",")
+    gps_coords = [float(coord) for coord in gps_coords]
+    return weather_now, weather_forcast, location, gps_coords
+
+def determineBirchPollenColor(pollen_count):
+    if pollen_count > 0 and pollen_count < 3:
+        return bcolors.GREEN
+    if pollen_count >= 3 and pollen_count < 20:
+        return bcolors.YELLOW
+    elif pollen_count >= 20 and pollen_count < 100:
+        return bcolors.RED
+    elif pollen_count >= 100:
+        return bcolors.MAGENTA
+
+def getLunarChineseChars(month, day):
+    chinese_months = [
+        "正月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "冬月", "腊月"
+    ]
+    chinese_days = [
+        "初一", "初二", "初三", "初四", "初五", "初六", "初七", "初八", "初九", "初十",
+        "十一", "十二", "十三", "十四", "十五", "十六", "十七", "十八", "十九", "二十",
+        "廿一", "廿二", "廿三", "廿四", "廿五", "廿六", "廿七", "廿八", "廿九", "三十"
+    ]
+    return chinese_months[month-1] + chinese_days[day-1]
+
+class weatherInfo:
+    def __init__(self, location_human):
+        self.location_human = location_human
+        self.weather_now = None
+        self.weather_forcast = None
+        self.location = None
+        self.gps_coords = None
+        self.last_update_time = None
+
+    def getDoI(self):
+        gps_coords = self.gps_coords
+        details_forcast     = subprocess.getoutput(f'curl -fGsS "https://api.open-meteo.com/v1/forecast?latitude={gps_coords[0]}&longitude={gps_coords[1]}&minutely_15=temperature_2m,apparent_temperature,precipitation,precipitation_probability&forecast_days=2&timezone=auto"')
+        details_air_quality = subprocess.getoutput(f'curl -fGsS "https://air-quality-api.open-meteo.com/v1/air-quality?latitude={gps_coords[0]}&longitude={gps_coords[1]}&hourly=birch_pollen,european_aqi&forecast_days=2&timezone=auto"')
+
+        data_forcast = json.loads(details_forcast)
+        data_air_quality = json.loads(details_air_quality)
+
+        tz = pytz.timezone(data_forcast["timezone"])
+        now = datetime.datetime.now(tz=tz)
+        df_forcast = pd.DataFrame({
+            "time": [tz.localize(datetime.datetime.fromisoformat(t)) for t in data_forcast["minutely_15"]["time"]],
+            "temperature_2m": data_forcast["minutely_15"]["temperature_2m"],
+            "apparent_temperature": data_forcast["minutely_15"]["apparent_temperature"],
+            "precipitation": data_forcast["minutely_15"]["precipitation"],
+            "precipitation_probability": data_forcast["minutely_15"]["precipitation_probability"]
+        })
+        df_air_quality = pd.DataFrame({
+            "time": [tz.localize(datetime.datetime.fromisoformat(t)) for t in data_air_quality["hourly"]["time"]],
+            "birch_pollen": data_air_quality["hourly"]["birch_pollen"],
+            "european_aqi": data_air_quality["hourly"]["european_aqi"]
+        })
+        # interpolate air quality data to 1min intervals
+        df_air_quality = df_air_quality.set_index("time").resample(datetime.timedelta(minutes=1)).interpolate().reset_index()
+        df_forcast = df_forcast[df_forcast["time"] >= now - datetime.timedelta(minutes=20)]
+        df_forcast = df_forcast[df_forcast["time"] < now + datetime.timedelta(days=1)]
+        df_air_quality = df_air_quality[df_air_quality["time"] >= now - datetime.timedelta(minutes=1)]
+        df_air_quality = df_air_quality[df_air_quality["time"] < now + datetime.timedelta(days=1)]
+        return df_forcast, df_air_quality
+
+    def getArt(self):
+        width = 128
+        date_str = None
+        now = datetime.datetime.now(tz=datetime.timezone.utc).astimezone()
+        if self.weather_now is None or (now - self.last_update_time).total_seconds() > 600:
+            self.weather_now, self.weather_forcast, self.location, self.gps_coords = getWeather(self.location_human)
+            self.last_update_time = now
+        date_str = now .strftime(f"%a  %d{determine_th_st_nd_rd(now.day)}  %b") 
+        date_art       = text2art(date_str, font='colossal')
+        lunar_date = LunarDate.today()
+        lunar_date_str = getLunarChineseChars(lunar_date.month, lunar_date.day)
+        hour_str = now.strftime("%H")
+        minute_str = now.strftime("%M")
+        dot_str = " : " if now.second % 2 == 0 else "   "
+        time_art = text2art(f'{hour_str}{dot_str}{minute_str}', font='colossal')
+
+        weather_now_print = f'                Updated {(now - self.last_update_time).total_seconds()/60:.0f}min ago\n' + self.weather_now
+        print_part_date = '\n' * (1 if self.location != "Unknown Location" else 10) + colour_art(centre_art(combine_arts([date_art, "\n"*8]), width-1), bcolors.CYAN) + '\n\n'
+        print_part_time = combine_arts(['                  \n'* 8, time_art, '   │\n'*8, '\n'*2+weather_now_print]) + '\n\n'
+        # print_part_weat = combine_arts([' \n'* 20, self.weather_forcast]) + '\n\n'
+        print_part_loca = centre_art(f'{self.location.strip()}, {lunar_date_str}', width-1)
+        print_part_all = print_part_loca + '\n\n' + print_part_date + print_part_time
+        
+        return print_part_all
