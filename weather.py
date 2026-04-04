22dcbb71c99f0495af6504e44b5de1bf1361f7cc
Qichen Dong <dqixol@icloud.com>
2026-04-04 22:58:02 +0100

    taste

--- weather.py
+++ weather.py
@@ -42,16 +42,16 @@ def determine_th_st_nd_rd(day):
     else: 
         return ["st", "nd", "rd"][day % 10 - 1]
 
-def getLunarChineseChars(month, day):
+def getLunarChineseChars(month, day, year=None):
     chinese_months = [
-        "正月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "冬月", "腊月"
+        "正月", "貳月", "參月", "肆月", "伍月", "陸月", "柒月", "捌月", "玖月", "拾月", "冬月", "腊月"
     ]
     chinese_days = [
-        "初一", "初二", "初三", "初四", "初五", "初六", "初七", "初八", "初九", "初十",
-        "十一", "十二", "十三", "十四", "十五", "十六", "十七", "十八", "十九", "二十",
-        "廿一", "廿二", "廿三", "廿四", "廿五", "廿六", "廿七", "廿八", "廿九", "三十"
+        "初壹", "初貳", "初參", "初肆", "初伍", "初陸", "初柒", "初捌", "初玖", "初拾",
+        "拾壹", "拾貳", "拾參", "拾肆", "拾伍", "拾陸", "拾柒", "拾捌", "拾玖", "貳拾",
+        "廿壹", "廿貳", "廿參", "廿肆", "廿伍", "廿陸", "廿柒", "廿捌", "廿玖", "參拾"
     ]
-    return chinese_months[month-1] + chinese_days[day-1]
+    return f'{chinese_months[month-1]}{chinese_days[day-1]}'
 class weatherInfo:
     def __init__(self, location_human):
         self.location_human = location_human
